f477fc22e8719fd2290379cb2598afbd43917ed6
Ryan Wolk <rwolk@hmc.edu>
2026-04-07 12:06:54 -0700

    B20 Test Generation

--- src/cover_float/cli.py
+++ src/cover_float/cli.py
@@ -82,6 +82,8 @@ def testgen() -> None:
         auto_parse("B14", args.output_dir)
         tg.B15.main()
         auto_parse("B15", args.output_dir)
+        tg.B20.main()
+        auto_parse("B20", args.output_dir)
         tg.B21.main()
         auto_parse("B21", args.output_dir)
     else:
@@ -124,6 +126,9 @@ def testgen() -> None:
         if "B15" in args.models:
             tg.B15.main()
             auto_parse("B15", args.output_dir)
+        if "B20" in args.models:
+            tg.B20.main()
+            auto_parse("B20", args.output_dir)
         if "B21" in args.models:
             tg.B21.main()
             auto_parse("B21", args.output_dir)
--- src/cover_float/testgen/__init__.py
+++ src/cover_float/testgen/__init__.py
@@ -11,6 +11,7 @@ import cover_float.testgen.B12 as B12
 import cover_float.testgen.B13 as B13
 import cover_float.testgen.B14 as B14
 import cover_float.testgen.B15 as B15
+import cover_float.testgen.B20 as B20
 import cover_float.testgen.B21 as B21
 
 __all__ = [
@@ -27,5 +28,6 @@ __all__ = [
     "B13",
     "B14",
     "B15",
+    "B20",
     "B21",
 ]
