d663ecd90b747b5bfa1302ef2c32775dd8d65f7a
sww0000 <143860457+sww0000@users.noreply.github.com>
2026-04-23 18:08:15 -0700

    added path (removed from resolving the wrong conflict :p)

--- src/cover_float/testgen/B14.py
+++ src/cover_float/testgen/B14.py
@@ -10,13 +10,15 @@
 import logging
 import random
 from random import seed
-from typing import TextIO, cast
+from pathlib import Path
+from typing import TextIO
 
 import cover_float.common.constants as const
 import cover_float.common.log as log
 from cover_float.common.util import reproducible_hash
 from cover_float.reference import run_and_store_test_vector
 from cover_float.testgen.model import register_model
+
 
 logger: log.ModelLogger = cast(log.ModelLogger, logging.getLogger("B14"))
 
