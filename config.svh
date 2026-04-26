1fab631ce93669e0406eab13c81a2e9785606082
sww0000 <143860457+sww0000@users.noreply.github.com>
2026-04-26 15:24:16 -0700

    Uncomment coverage definitions in config.svh

--- config.svh
+++ config.svh
@@ -1,35 +1,35 @@
 // define macros for which models to check coverage for
 // e.g. `define COVER_B1 for model B1
 
-// `define COVER_B1
-// `define COVER_B2
-// `define COVER_B3
-// `define COVER_B4
-// `define COVER_B5
-// `define COVER_B6
-// `define COVER_B7
-// `define COVER_B8
-// `define COVER_B9
-// `define COVER_B10
-// `define COVER_B11
-// `define COVER_B12
-// `define COVER_B13
+`define COVER_B1
+`define COVER_B2
+`define COVER_B3
+`define COVER_B4
+`define COVER_B5
+`define COVER_B6
+`define COVER_B7
+`define COVER_B8
+`define COVER_B9
+`define COVER_B10
+`define COVER_B11
+`define COVER_B12
+`define COVER_B13
 `define COVER_B14
-// `define COVER_B15
-// `define COVER_B16
-// `define COVER_B17
-// `define COVER_B18
-// `define COVER_B19
-// `define COVER_B20
-// `define COVER_B21
-// `define COVER_B22
-// `define COVER_B23
+`define COVER_B15
+`define COVER_B16
+`define COVER_B17
+`define COVER_B18
+`define COVER_B19
+`define COVER_B20
+`define COVER_B21
+`define COVER_B22
+`define COVER_B23
 `define COVER_B24
-// `define COVER_B25
-// `define COVER_B26
-// `define COVER_B27
-// `define COVER_B28
-// `define COVER_B29
+`define COVER_B25
+`define COVER_B26
+`define COVER_B27
+`define COVER_B28
+`define COVER_B29
 
 // define macros for which precisions to check coverage for
 // e.g. `define COVER_F32 for single precision
